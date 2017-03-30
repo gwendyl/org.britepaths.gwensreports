72dc8b152dda0581a9ecfe45c94a449145c77845
Andrew Hunt <andrew@aghstrategies.com>
2017-03-30 15:16:14 -0400

    make contact type default

--- CRM/Gwensreports/Form/Report/Contribute/Depdate.php
+++ CRM/Gwensreports/Form/Report/Contribute/Depdate.php
@@ -1,5 +1,8 @@
 <?php
 
 class CRM_Gwensreports_Form_Report_Contribute_Depdate extends CRM_Report_Form_Contribute_Detail {
+  public function __construct() {
+    $this->_columns['civicrm_contact']['fields']['contact_type']['default'] = TRUE;
+  }
 
 }
