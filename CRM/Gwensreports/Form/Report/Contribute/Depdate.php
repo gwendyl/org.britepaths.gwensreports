3fa27e752d1e8b27f46737cbd41a2bcb5cd83205
Andrew Hunt <andrew@aghstrategies.com>
2017-03-30 15:18:54 -0400

    oops skipped all construct

--- CRM/Gwensreports/Form/Report/Contribute/Depdate.php
+++ CRM/Gwensreports/Form/Report/Contribute/Depdate.php
@@ -2,6 +2,7 @@
 
 class CRM_Gwensreports_Form_Report_Contribute_Depdate extends CRM_Report_Form_Contribute_Detail {
   public function __construct() {
+    parent::__construct();
     $this->_columns['civicrm_contact']['fields']['contact_type']['default'] = TRUE;
   }
 
