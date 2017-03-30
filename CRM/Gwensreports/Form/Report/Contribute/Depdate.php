83fd50060e569a5235cda5d7eb1847bc29aa11bf
Andrew Hunt <andrew@aghstrategies.com>
2017-03-30 15:29:35 -0400

    unset defaults

--- CRM/Gwensreports/Form/Report/Contribute/Depdate.php
+++ CRM/Gwensreports/Form/Report/Contribute/Depdate.php
@@ -4,6 +4,14 @@ class CRM_Gwensreports_Form_Report_Contribute_Depdate extends CRM_Report_Form_Co
   public function __construct() {
     parent::__construct();
     $this->_columns['civicrm_contact']['fields']['contact_type']['default'] = TRUE;
+    $this->_columns['civicrm_phone']['fields']['phone']['default'] = FALSE;
+    $this->_columns['civicrm_email']['fields']['email']['default'] = FALSE;
+    if (!empty($this->_columns['civicrm_contribution']['fields']['campaign_id'])) {
+      $this->_columns['civicrm_contribution']['fields']['campaign_id']['default'] = FALSE;
+    }
+    if (!empty($this->_columns['civicrm_address']['fields']['country_id'])) {
+      $this->_columns['civicrm_contribution']['fields']['country_id']['default'] = FALSE;
+    }
   }
 
 }
