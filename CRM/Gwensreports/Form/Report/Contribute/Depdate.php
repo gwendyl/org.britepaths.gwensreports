8c6f2625103b2d8a493dbde495e4406376ea8fdf
Andrew Hunt <andrew@aghstrategies.com>
2017-03-30 15:52:23 -0400

    rearrange column order

--- CRM/Gwensreports/Form/Report/Contribute/Depdate.php
+++ CRM/Gwensreports/Form/Report/Contribute/Depdate.php
@@ -14,4 +14,24 @@ class CRM_Gwensreports_Form_Report_Contribute_Depdate extends CRM_Report_Form_Co
     }
   }
 
+  public function alterDisplay(&$rows) {
+    parent::alterDisplay($rows);
+
+    // Put contact type next to donor category field
+    if (!empty($this->_columnHeaders['civicrm_contact_contact_type'])
+    && !empty($this->_columnHeaders['civicrm_value_donor_type_19_custom_142'])) {
+      $newColumnHeaders = array();
+      $contactTypeCol = $this->_columnHeaders['civicrm_contact_contact_type'];
+      unset($this->_columnHeaders['civicrm_contact_contact_type']);
+      foreach ($this->_columnHeaders as $key => $col) {
+        if ($key == 'civicrm_value_donor_type_19_custom_142') {
+          $newColumnHeaders['civicrm_contact_contact_type'] = $contactTypeCol;
+          $contactTypeCol = FALSE;
+        }
+        $newColumnHeaders[$key] = $col;
+      }
+      $this->_columnHeaders = $newColumnHeaders;
+    }
+  }
+
 }
