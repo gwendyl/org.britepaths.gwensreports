ad86c048a6527d4856c3a439355b13725f55cd39
Gwendyl Harter <gwendyl@gmail.com>
2017-04-04 10:33:06 -0400

    receive_date no longer defaults

--- CRM/Gwensreports/Form/Report/Contribute/Depdate.php
+++ CRM/Gwensreports/Form/Report/Contribute/Depdate.php
@@ -9,6 +9,9 @@ class CRM_Gwensreports_Form_Report_Contribute_Depdate extends CRM_Report_Form_Co
     if (!empty($this->_columns['civicrm_contribution']['fields']['campaign_id'])) {
       $this->_columns['civicrm_contribution']['fields']['campaign_id']['default'] = FALSE;
     }
+      if (!empty($this->_columns['civicrm_contribution']['fields']['receive_date'])) {
+          $this->_columns['civicrm_contribution']['fields']['receive_date']['default'] = FALSE;
+      }
     if (!empty($this->_columns['civicrm_address']['fields']['country_id'])) {
       $this->_columns['civicrm_address']['fields']['country_id']['default'] = FALSE;
     }
